3e40add52b3d869a67827b03378a0ae8dd84b7c2
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-17 04:28:07 -0700

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python3
-
+"""Defines the class BaseModel"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-	def __init__(self, *args, **kwargs):
-    	if kwargs:
-        	for key, value in kwargs.items():
-            	if key != '__class__':
-                	if key in ('created_at', 'updated_at'):
-                    	value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                	setattr(self, key, value)
+    """the BaseModel of the HBnB project."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key pairs of attributes.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, value)
     	else:
-        	self.id = str(uuid.uuid4())
-        	self.created_at = datetime.now()
-        	self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-	def __str__(self):
-    	return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def __str__(self):
+        """Return the str reps of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-	def save(self):
-    	self.updated_at = datetime.now()
+    def save(self):
+        """updating with current time """
+        self.updated_at = datetime.now()
 
-	def to_dict(self):
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance"""
     	obj_dict = self.__dict__.copy()
     	obj_dict['__class__'] = self.__class__.__name__
     	obj_dict['created_at'] = self.created_at.isoformat()
