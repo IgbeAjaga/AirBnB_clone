5fd75ce21c62e2989bd2e7264433ad3798531e6d
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-17 08:04:09 -0700

    update'
    git commit -m update

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the class BaseModel"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -24,6 +24,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """Return the str reps of the BaseModel instance."""
@@ -32,6 +34,7 @@ class BaseModel:
     def save(self):
         """updating with current time """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance"""
