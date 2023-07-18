ca492ba0ff8ee99927eeaaf1b7357a211d8725ca
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-18 09:28:47 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -7,25 +7,24 @@ import models
 
 class BaseModel:
     """the BaseModel of the HBnB project."""
-
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key pairs of attributes.
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                        setattr(self, key, value)
-    	else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            models.storage.new(self)
+    """Initialize a new BaseModel.
+    Args:
+    *args (any): Unused.
+    **kwargs (dict): Key pairs of attributes.
+    """
+    if kwargs:
+        for key, value in kwargs.items():
+            if key != '__class__':
+                if key in ('created_at', 'updated_at'):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = datetime.now()
+                else:
+                    models.storage.new(self)
 
     def __str__(self):
         """Return the str reps of the BaseModel instance."""
