7e88577648429799f147c86208811033fb1e8e64
sweetpotatoX <67162997+sweetpotatoX@users.noreply.github.com>
2023-07-19 03:11:38 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -5,17 +5,8 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """Represent an amenity.
-
     Attributes:
         name (str): The name of the amenity.
     """
 
-    name = BaseModel.StringField(required=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = kwargs.get("name", "")
-
-    def __repr__(self):
-        return "Amenity(id={}, name={})".format(self.id, self.name)
-
+    name = ""
